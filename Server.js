675a7ce9baa42eff4dc329011b55f4662dd2f464
Walter Ebuka Onyekwelu <onyekweluwalter@gmail.com>
2024-10-30 22:37:00 +0100

    Delete Server.js

--- Server.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-
-const connectDB = async ()=>{
-    mongoose.connect(`${process.env.MONGODB_UL }`)
-    .then(()=> console.log("My MongoDB Connected..!"))
-}
-
-module.exports = connectDB
