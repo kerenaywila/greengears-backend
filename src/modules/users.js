985d42b6f07c1ebe8ce4b3930010849f291be079
Walter <onyekweluwalter@gmail.com>
2024-10-31 22:10:18 +0100

    Updated admin

--- src/modules/users.js
+++ src/modules/users.js
@@ -25,30 +25,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Define the Admin schema
-const adminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true },
-  contactNumber: { type: String, required: true },
-  role: { type: String, default: 'admin'},
-  permissions: {
-    manageUsers: { type: Boolean, default: false },
-    manageEquipment: { type: Boolean, default: false },
-    viewReports: { type: Boolean, default: false },
-    managePayments: { type: Boolean, default: false },
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
 
 
 const User = mongoose.model('User', userSchema);
-const Admin = mongoose.model('Admin', adminSchema);
 
-module.exports =  { User , Admin }
+module.exports =  User;
 // Set up Node.js project with Express and Mongoose.
 // Configure MongoDB (Atlas/local).
 // Implement User Signup API for User and Admin with OTP email verification using Nodemailer.
