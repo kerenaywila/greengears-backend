2d31e66f7f498e4c5526baf869cce081d7aeb74b
kerenaywila <kerensss64@gmail.com>
2024-11-13 03:11:56 +0100

    added multer

--- src/modules/equipment.js
+++ src/modules/equipment.js
@@ -5,15 +5,20 @@ const Schema = mongoose.Schema
 
 
 const equipmentSchema = new mongoose.Schema({
-    name: String,
-    type: String,
-    brand: String,
-    serialNumber: String,
-    purchaseDate: Date,
-    price: Number,
-    description: String,
-    location: String, 
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    brand: { type: String, required: true },
+    serialNumber: {type: String},
+    purchaseDate: {type: Date},
+    price: { type: Number, required: true },
+    available: { type: Boolean, default: true },
+    description: { type: String },
+    location: { type: String },
+    images: [{ type: String }],  // Array of image URLs 
 });
+
+// Indexing for faster queries
+equipmentSchema.index({ price: 1, location: 1 });
 
 const Equipment = mongoose.model("Equipment", equipmentSchema);
 module.exports = Equipment
--- src/routes/equipment.js
+++ src/routes/equipment.js
@@ -1,10 +1,38 @@
 // routes/equipment.js
 const express = require("express");
 const { equipmentFxn } = require("../controllers/equipmentController");
+const upload = require('../middleware/multer');
+const Equipment = require('../models/equipment');
 
 const router = express.Router();
 
 // GET /api/equipment?page=1&limit=10&type=example&minPrice=100&maxPrice=500&location=NewYork
 router.get("/equipment", equipmentFxn )
+router.get("/single/equipment/:id", getSingleEquipment )
+
+router.post('/equipment/:id/upload', upload, async (req, res) => {
+    try {
+      const equipment = await Equipment.findById(req.params.id);
+  
+      if (!equipment) {
+        return res.status(404).json({ message: 'Equipment not found' });
+      }
+  
+      const imageUrls = req.files.map(file => file.path);
+  
+      equipment.images.push(...imageUrls);
+  
+      await equipment.save();
+  
+      res.status(200).json({
+        message: 'Images uploaded successfully',
+        images: imageUrls
+      });
+    } catch (error) {
+      res.status(500).json({error: error.message });
+    }
+  });
     
+
+
 module.exports = router;
