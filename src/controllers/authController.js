46810ebc0e4f3cd845209bb17bf93edcc07d0f09
Walter <onyekweluwalter@gmail.com>
2024-11-29 07:52:54 +0100

    User Activated added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -308,7 +308,25 @@ exports.resendOtp_admin = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+//ACTIVATE USER
 
+exports.activateUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await Farmer.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.isActive = true;
+    await user.save();
+
+    return res.status(200).json({ message: "Successful", user });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 // DEACTIVATE USER
 exports.deactivateUser = async (req, res) => {
     try {
